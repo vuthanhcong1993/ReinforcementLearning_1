3c60c7a74d084ed1b0890774eecc1b2d8c90f5f5
vuthanhcong1996 <vuthanhcong96@gmail.com>
2019-01-20 17:18:58 +0700

    First Commit

---
+++ ml-agents-master/UnitySDK/Assets/Test.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.Serialization;
+
+
+public class Test : MonoBehaviour
+{
+    [FormerlySerializedAs("gg")]
+    public float health = 50;
+
+    public float gg = 6;
+    public Transform cube;
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Sets the rotation so that the transform's y-axis goes along the z-axis
+        
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        transform.rotation = Quaternion.FromToRotation(transform.up, cube.forward);
+    }
+    [MenuItem("gg/tt")]
+    static void Mama()
+    {
+        Debug.Log("xx");
+    }
+}
